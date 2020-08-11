4127133a377ccfffc831ef1dd2b4649fe9aef7ad
hh-jang <jhh1117@daum.net>
2020-08-11 13:49:17 +0900

    Google OAuth config 추가
    * key.yml 등록하여 관리, git을 통해 형상관리하지 않음
    
    * spring 기동시 해당 key 정보를 로드하도록 수정

--- src/main/java/com/hhjang/security/springsecuritystudy/SpringSecurityStudyApplication.java
+++ src/main/java/com/hhjang/security/springsecuritystudy/SpringSecurityStudyApplication.java
@@ -2,12 +2,17 @@ package com.hhjang.security.springsecuritystudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class SpringSecurityStudyApplication {
 
+    private static final String KEY_PROPERTIES = "spring.config.location=classpath:/key.yml";
+
     public static void main(String[] args) {
-        SpringApplication.run(SpringSecurityStudyApplication.class, args);
+        new SpringApplicationBuilder(SpringSecurityStudyApplication.class)
+                .properties(KEY_PROPERTIES)
+                .run(args);
     }
 
 }

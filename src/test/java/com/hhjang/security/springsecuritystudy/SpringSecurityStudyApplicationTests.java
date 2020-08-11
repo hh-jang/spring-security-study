d395b383155bf2c1115fab4e3a906b6a2280f049
hh-jang <jhh1117@daum.net>
2020-08-11 12:39:07 +0900

    init project

---
+++ src/test/java/com/hhjang/security/springsecuritystudy/SpringSecurityStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhjang.security.springsecuritystudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
